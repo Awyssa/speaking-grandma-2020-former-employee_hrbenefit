3460a4c5bb634d138d6d9588577cd03c74c7a324
Michael Ernest <m.hendersun@gmail.com>
2020-09-29 16:27:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(p)
     return "I LOVE YOU TOO PUMPKIN!"
   end
 
-  if p = p.downcase()
+  if p == p.downcase()
     return 'HUH?! SPEAK UP, SONNY!'
   else
     return "NO, NOT SINCE 1938!"
